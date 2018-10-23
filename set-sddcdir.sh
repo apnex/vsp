a19a6ff2a3931be2d3e1dd4106d5521863d811b9
Andrew Obersnel <apnex@users.noreply.github.com>
2018-10-23 11:36:05 +1100

    Added bash auto-completion file

--- set-sddcdir.sh
+++ set-sddcdir.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+echo "export SDDCDIR = [${PWD}/cfg]"
 export SDDCDIR="${PWD}/cfg"
+
+# copy bash completions to /etc/bash_completion.d/
+# auto-generate completion.bash file?
+for FILE in ${PWD}/*; do
+	if [[ $FILE =~ ([^/]+completion[.]bash)$ ]]; then
+		echo "install [${BASH_REMATCH[1]}] to [/etc/bash_completion.d/]"
+		\cp -f "$FILE" /etc/bash_completion.d/
+	fi
+done
